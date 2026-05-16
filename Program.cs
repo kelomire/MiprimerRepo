58499e314bbae49039ef6e74815fef4d777f6c29
kelomire <leonhuapayaanderson@gmail.com>
2026-05-16 16:25:21 -0300

    mensaje del cambio

--- Program.cs
+++ Program.cs
@@ -1,1 +1,9 @@
-﻿
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hola Mundo");
+    }
+}
--- obj/Debug/net8.0/Hola.AssemblyInfo.cs
+++ obj/Debug/net8.0/Hola.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Hola")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d2cce791018c8dde39853559b10d09120d835ad3")]
 [assembly: System.Reflection.AssemblyProductAttribute("Hola")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Hola")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
